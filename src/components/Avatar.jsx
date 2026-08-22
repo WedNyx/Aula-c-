17f8561c66b0b202c695f156af29380cddc02f03
WedNyx <zackyt370@gmail.com>
2026-08-22 19:31:49 -0300

    Corrige abertura dos perfis com pets

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useRef } from "react";
 import { createAvatar } from "@dicebear/core";
 import { bigSmile } from "@dicebear/collection";
 import { shade } from "../lib/colors.ts";
